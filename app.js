fb506af045bfc93715bb471b0f54e94f595bc598
Julie Lewis <julieahoward@gmail.com>
2018-05-23 09:09:49 +1200

    Fixing autocomplete, Cleaning up code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelectorAll('#map').length > 0)
-  {
+
+//Checks to see if map exists on page, if true - inserts API in head
+  if (document.querySelectorAll('#map').length > 0){
     if (document.querySelector('html').lang)
       lang = document.querySelector('html').lang;
     else
@@ -8,62 +9,71 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAJy7fFz2MNZ5zlNnpbG0FnDHlWJUClNsc&signed_in=true&libraries=places&callback=initialize&language=' + lang;
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAJy7fFz2MNZ5zlNnpbG0FnDHlWJUClNsc&libraries=places&callback=initialize&language=' + lang;
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });
 
+//Allows both map and autocomplete callbacks
 function initialize() {
    initMap();
-   initAutoComplete();
+   initAutocomplete();
 }
 
+// ********************************************
+// SET MAP
+// ********************************************
 var map;
 
 function initMap()
 {
+  //Creates new map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 8
   });
 
+  //Creates cluster markers, using cluster icons from Image folder
   var markerCluster = new MarkerClusterer(map, markers,
           {imagePath: 'images'});
 
-  // Gets then renders JSON file data, per plotMarkers function
+  // Gets and renders JSON file data, per plotMarkers function
   fetch('markers.json')
     .then(function(response){return response.json()})
     .then(plotMarkers);
 
-
 }
 
+// ********************************************
+// PLOT MARKERS
+// ********************************************
 var markers;
-var bounds;
 
 function plotMarkers(m)
 {
   markers = [];
-  bounds = new google.maps.LatLngBounds();
+  let bounds = new google.maps.LatLngBounds();
 
   m.forEach(function (marker) {
+    //sets position of marker, based on lng/lat from object
     var position = new google.maps.LatLng(marker.lat, marker.lng);
-    var name = '<h2>' + marker.name + '</h2>' +
+    //sets content of infoWindow, to be called later
+    var infoContent = '<h2>' + marker.name + '</h2>' +
                '<p>Phone: <a href="tel:' + marker.phone + '">' + marker.phone + '</a></p>' +
                 '<div id="contactButton">' +
                   '<button>Request a Quote</button>' +
                 '</div>'
     ;
 
-    var marker =
+    marker =
       new google.maps.Marker({
         position: position,
         map: map,
-        info: name,
+        info: infoContent,
         animation: google.maps.Animation.DROP
       });
 
-    infoWindow = new google.maps.InfoWindow({ content: name });
+    infoWindow = new google.maps.InfoWindow({ content: infoContent });
 
     marker.addListener('mouseover', function() {
        infoWindow.setContent( this.info );
@@ -79,97 +89,91 @@ function plotMarkers(m)
 }
 
 
-  // ********************************************
-  // VARIABLES
-  // ********************************************
+// ********************************************
+// MODAL CONTROLS
+// ********************************************
+
+// Open Modal on click
+$("#map").on('click', '#contactButton', e => {
   const overlayWrap = document.getElementById('overlay-wrapper');
+  overlayWrap.style.display = "block";
+});
 
-  // ********************************************
-  // OPEN MODAL ON CLICK
-  // ********************************************
-  $("#map").on('click', '#contactButton', e => {
-    overlayWrap.style.display = "block";
-  });
+// Close modal by clicking X
+$('#overlay-wrapper').on('click', '#close', e=> { // Delegated event handler to existing DOM element
+  $("#overlay-wrapper").css("display", "none");
+});
 
-  // ********************************************
-  // MODAL CONTROLS
-  // ********************************************
-  // Close modal by clicking X
-  $('#overlay-wrapper').on('click', '#close', e=> { // Delegated event handler to existing DOM element
+// Close modal by clicking outside of modal
+window.onclick = function(event) {
+  if (event.target == overlayWrap) {
     $("#overlay-wrapper").css("display", "none");
-  });
+  }
+};
 
-  // Close modal by clicking outside of modal
-  window.onclick = function(event) {
-    if (event.target == overlayWrap) {
-      $("#overlay-wrapper").css("display", "none");
-    }
-  };
+// ********************************************
+// ADDRESS AUTOCOMPLETE SETTINGS
+// ********************************************
 
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
 
-  // This example displays an address form, using the autocomplete feature
-  // of the Google Places API to help users fill in the information.
+var placeSearch, autocomplete;
+var componentForm = {
+  street_number: 'short_name',
+  route: 'long_name',
+  locality: 'long_name',
+  administrative_area_level_1: 'short_name',
+  country: 'long_name',
+  postal_code: 'short_name'
+};
 
-  // This example requires the Places library. Include the libraries=places
-  // parameter when you first load the API. For example:
-  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+      {types: ['geocode']});
 
-  var placeSearch, autocomplete;
-  var componentForm = {
-    street_number: 'short_name',
-    route: 'long_name',
-    locality: 'long_name',
-    administrative_area_level_1: 'short_name',
-    country: 'long_name',
-    postal_code: 'short_name'
-  };
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
 
-  function initAutocomplete() {
-    // Create the autocomplete object, restricting the search to geographical
-    // location types.
-    autocomplete = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-        {types: ['geocode']});
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
 
-    // When the user selects an address from the dropdown, populate the address
-    // fields in the form.
-    autocomplete.addListener('place_changed', fillInAddress);
+  for (var component in componentForm) {
+    document.getElementById(component).value = '';
+    document.getElementById(component).disabled = false;
   }
 
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-      document.getElementById(component).disabled = false;
-    }
-
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
+  // Get each component of the address from the place details
+  // and fill the corresponding field on the form.
+  for (var i = 0; i < place.address_components.length; i++) {
+    var addressType = place.address_components[i].types[0];
+    if (componentForm[addressType]) {
+      var val = place.address_components[i][componentForm[addressType]];
+      document.getElementById(addressType).value = val;
     }
   }
+}
 
-  // Bias the autocomplete object to the user's geographical location,
-  // as supplied by the browser's 'navigator.geolocation' object.
-  function geolocate() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var geolocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        var circle = new google.maps.Circle({
-          center: geolocation,
-          radius: position.coords.accuracy
-        });
-        autocomplete.setBounds(circle.getBounds());
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
       });
-    }
+      autocomplete.setBounds(circle.getBounds());
+    });
   }
+}
